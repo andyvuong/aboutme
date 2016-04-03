c192cd412a9d66e84a036005ea9bb91a332e2e01
andyvuong <andyv.42@gmail.com>
2016-04-02 22:30:30 -0500

    Fix word phrase for proper grammar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#target').teletype({text: ['software engineer.', 'computer scientist.', 
-                                'student.', 'explorer.', 'sci-fi lover.']});
+                                'student.', 'curious explorer.', 'sci-fi lover.']});
     $('#cursor').teletype({text: ['|', ' '], delay: 0, pause: 250});
 });
