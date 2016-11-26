9fd40bfccf132f836bc5fb6e17a78d7b46f6ef3b
andyvuong <andyv.42@gmail.com>
2016-11-26 02:22:58 -0600

    update website

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
 $(document).ready(function() {
-    $('#target').teletype({text: ['software engineer.', 'computer scientist.', 
-                                'web developer.', 'curious explorer.', 'sci-fi lover.']});
-    $('#cursor').teletype({text: ['|', ' '], delay: 0, pause: 250});
+
 });
